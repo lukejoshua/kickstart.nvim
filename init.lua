674fa1f9baaba7c1c614f513dfe20d2424d3f8c6
Luke Joshua <luke.joshua.dev@gmail.com>
2024-01-03 10:46:28 +0200

    feat: add transparency

--- init.lua
+++ init.lua
@@ -82,6 +82,9 @@ vim.o.completeopt = 'menuone,noselect'
 
 vim.o.termguicolors = true
 
+-- Transparency
+vim.cmd 'highlight Normal ctermbg=NONE guibg=NONE'
+
 -- [[ Basic Keymaps ]]
 
 -- Keymaps for better default experience
