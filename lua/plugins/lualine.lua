aad546847bf57c996f005e0602a12c5a870785bf
Luke Joshua <luke.joshua.dev@gmail.com>
2024-01-11 16:54:46 +0200

    feat: customize lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,12 +2,45 @@ return {
   -- Set lualine as statusline
   'nvim-lualine/lualine.nvim',
   -- See `:help lualine.txt`
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
   opts = {
     options = {
-      icons_enabled = false,
+      icons_enabled = true,
       theme = 'catppuccin',
-      component_separators = '|',
-      section_separators = '',
+      component_separators = { left = '', right = '' },
+      section_separators = { left = '', right = '' },
+      globalstatus = true,
+    },
+    sections = {
+      lualine_a = { 'mode' },
+      lualine_b = { 'branch' },
+      lualine_c = { 'filename' },
+      lualine_x = { 'filetype' },
+      lualine_y = { 'diagnostics' },
+      lualine_z = {
+        {
+          'location',
+          fmt = function(str)
+            -- Remove the column
+            return 'Line ' .. string.gsub(str, '%:.+$', '')
+          end,
+        },
+        {
+          'location',
+          fmt = function(str)
+            -- Remove the line
+            return 'Column ' .. string.gsub(str, '^.+%:', '')
+          end,
+        },
+      },
+    },
+    inactive_sections = {
+      lualine_a = {},
+      lualine_b = {},
+      lualine_c = { 'filename' },
+      lualine_x = { 'location' },
+      lualine_y = {},
+      lualine_z = {},
     },
   },
 }
