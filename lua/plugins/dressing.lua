a968242fe2adf6fab2de64c21a946a46ad6cd5a2
Luke Joshua <luke.joshua.dev@gmail.com>
2024-01-11 16:53:53 +0200

    feat: add dressing.nvim (disabled)

---
+++ lua/plugins/dressing.lua
@@ -0,0 +1,41 @@
+return {
+  enabled = false,
+  'stevearc/dressing.nvim',
+  lazy = true,
+  init = function()
+    ---@diagnostic disable-next-line: duplicate-set-field
+    vim.ui.select = function(...)
+      require('lazy').load { plugins = { 'dressing.nvim' } }
+      return vim.ui.select(...)
+    end
+    ---@diagnostic disable-next-line: duplicate-set-field
+    vim.ui.input = function(...)
+      require('lazy').load { plugins = { 'dressing.nvim' } }
+      return vim.ui.input(...)
+    end
+  end,
+  config = function()
+    require('dressing').setup {
+      select = {
+        backend = { 'builtin' },
+        get_config = function(opts)
+          if opts.kind == 'codeaction' then
+            return {
+              builtin = {
+                relative = 'cursor',
+              },
+            }
+          end
+
+          if opts.kind == 'ngswitch' then
+            return {
+              builtin = {
+                relative = 'win',
+              },
+            }
+          end
+        end,
+      },
+    }
+  end,
+}
