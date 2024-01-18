96b23036c352a644b10e16c4eacb56169fd3c719
Luke Joshua <luke.joshua.dev@gmail.com>
2024-01-18 20:47:41 +0200

    feat: add conform

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,28 @@
+return {
+  'stevearc/conform.nvim',
+  event = 'BufEnter',
+  opts = {
+    formatters_by_ft = {
+      ['javascript'] = { 'prettierd' },
+      ['javascriptreact'] = { 'prettierd' },
+      ['typescript'] = { 'prettierd' },
+      ['typescriptreact'] = { 'prettierd' },
+      ['vue'] = { 'prettierd' },
+      ['css'] = { 'prettierd' },
+      ['scss'] = { 'prettierd' },
+      ['less'] = { 'prettierd' },
+      ['html'] = { 'prettierd' },
+      ['json'] = { 'prettierd' },
+      ['jsonc'] = { 'prettierd' },
+      ['yaml'] = { 'prettierd' },
+      ['markdown'] = { 'prettierd' },
+      ['markdown.mdx'] = { 'prettierd' },
+      ['graphql'] = { 'prettierd' },
+      ['handlebars'] = { 'prettierd' },
+    },
+    format_on_save = {
+      lsp_fallback = true,
+      timeout_ms = 500,
+    },
+  },
+}
